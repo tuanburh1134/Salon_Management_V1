04b35a35fc3844a0dde0f57a11f0a5f46b2a6e8f
NguyenPhuTrongBIT27082005 <phutrong27082005@gmail.com>
2025-10-23 10:44:08 +0700

    them customerService

--- src/main/java/com/example/salon_management/service/CustomerService.java
+++ src/main/java/com/example/salon_management/service/CustomerService.java
@@ -1,4 +1,13 @@
-package example.salon_management.service;
+package com.example.salon_management.service;
 
-public class CustomerService {
+import com.example.salon_management.dto.CustomerForm;
+import com.example.salon_management.entity.Customer;
+import org.springframework.data.domain.*;
+
+public interface CustomerService {
+    Page<Customer> search(String keyword, Pageable pageable);
+    Customer get(Long id);
+    void create(CustomerForm form);
+    void update(Long id, CustomerForm form);
+    void delete(Long id);
 }
